56a68c380070df54f27e959e061c2e368c30c4ba
MAngelGC <magc6102@uma.es>
2023-12-21 04:08:18 +0100

    gitignore

--- BAM1.m
+++ BAM1.m
@@ -1,28 +1,37 @@
 clc;
 clear;
 
-X(1, :) = [1 1 1 -1 1 -1 -1 1 -1];
-X(2, :) = [1 -1 -1 1 -1 -1 1 1 1];
-Y(1, :) = [1 -1 -1];
-Y(2, :) = [-1 -1 1];
-W = X' * Y;
+X(1, :) = [1 1 1 -1 1 -1 -1 1 -1]; % Patron 1 de entrada
+X(2, :) = [1 -1 -1 1 -1 -1 1 1 1]; % Patron 2 de entrada
+Y(1, :) = [1 -1 -1]; % Patron de salida al que se asocia el patron 1
+Y(2, :) = [-1 -1 1]; % Patron de salida al que se asocia el patron 2
+W = X' * Y; % Calculo del vector de pesos
 
-KX = size(X, 2);
-KY = size(Y, 2);
+KX = size(X, 2); % Tamaño de los vectores de entrada
+KY = size(Y, 2); % Tamaño de los vectores de salida
 
-epochs = 21;
+epochs = 21; % Numero de iteraciones
 
-SX = zeros(KX, epochs);
-SY = zeros(KY, epochs);
+SX = zeros(KX, epochs); % Historico de salidas para los patrones de entrada
+SY = zeros(KY, epochs); % Historico de salidas para los patrones de salida
 
-SX(:, 1) = X(1, :);
-SY(:, 1) = sign(SX(:, 1)' * W);
+SX(:, 1) = X(1, :); % Se establece la primera salida de los patrones de 
+% entrada al patron 1 (el que queremos memorizar en este caso)
+SY(:, 1) = sign(SX(:, 1)' * W); % Se establece la primera salida de los
+% patrones de salida al resultado de la funcion signo de multiplicar el 
+% primer patron de entrada al vector de pesos calculado
 
 for i = 2:epochs
-    SX(:, i) = sign(W * SY(:, i - 1));
-    SY(:, i) = sign(SX(:, i)' * W);
+    SX(:, i) = sign(W * SY(:, i - 1)); % Se calcula la salida de los
+    % patrones de entrada en funcion del vector de pesos y la salida de
+    % los patrones de salida
+    SY(:, i) = sign(SX(:, i)' * W); % Con la salida de los patrones de 
+    % entrada calculada, se calcula la de los patrones de salida (junto
+    % con el vector de pesos)
     if (sum(SX(:, epochs) == SX(:, epochs - 1)) == KX && ...
-        sum(SY(:, epochs) == SY(:, epochs - 1)) == KY)
+        sum(SY(:, epochs) == SY(:, epochs - 1)) == KY) % Si se estabiliza
+        % la red (es decir, los estados de entrada y salida actuales son
+        % iguales a los anteriores), se para la ejecucion
         break;
     end
 end
